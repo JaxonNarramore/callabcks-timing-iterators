73f09d2982c932934953e4a7ad132c2d461b757d
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-10-22 09:18:48 -0500

    [add] Add to github"

---
+++ checknumber.js
@@ -0,0 +1,59 @@
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(5));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+console.log(checkNumber(10))
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(-20));
+
+function checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(40))
+
+functon checkNumber(num) {
+    if (num > 0) {
+        return 'positive';
+    } else if (num < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}
+
+console.log(checkNumber(-100));
---
+++ hello.js
@@ -0,0 +1,20 @@
+function sayHello(name) {
+    let msg = "hello, " + name + ". How are you?";
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
+
+function sayHello(name) {
+    let msg = 'hello, ' + name + '. How are you?';
+    return msg;
+}
+
+console.log(sayHello('SEI 1019'));
